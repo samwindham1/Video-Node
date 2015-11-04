b1c8c8f812d58ff295530c2e9ec2cbd9a3e6d967
samwindham1 <sam.windham95@gmail.com>
2015-11-03 18:34:54 -0700

    AngularJS fixes. Converted search function to angular.

--- app/js/app.js
+++ app/js/app.js
@@ -2,14 +2,53 @@
   var app = angular.module('VideoNode', []);
 
   app.controller('MovieController', function() {
-    this.movies = movieTable;
+    this.movies = library;
   });
 
-  var movieTable = [
-    { title : 'One', year : 2001 },
-    { title : 'Two', year : 2002 },
-    { title : 'Three', year : 2003 },
-    { title : 'Four', year : 2004 }
-  ];
+
+  // search box
+  app.controller('SearchController', ['$scope', function($scope) {
+    $scope.query = '';
+    $scope.text = 'Search...';
+    $scope.setQuery = function() {
+      if($scope.text) {
+        $scope.query = $scope.text;
+      }
+    };
+    $scope.blurOnEnter = function($event) {
+      var target = $event.target;
+      $scope.text = 'Search...';
+      target.blur();
+    };
+    $scope.clear = function() {
+      $scope.text = '';
+    }
+  }]);
+
+  var library = [{
+      title : 'Fight Club',
+      year : 1999,
+      images : [
+        "http://ia.media-imdb.com/images/M/MV5BMjIwNTYzMzE1M15BMl5BanBnXkFtZTcwOTE5Mzg3OA@@._V1_UY1200_CR88,0,630,1200_AL_.jpg"
+      ]
+    }, {
+      title : 'Silence of the Lambs',
+      year : 1991,
+      images : [
+        "http://ia.media-imdb.com/images/M/MV5BMTQ2NzkzMDI4OF5BMl5BanBnXkFtZTcwMDA0NzE1NA@@._V1_SX640_SY720_.jpg"
+      ]
+    }, {
+      title : 'Django Unchained',
+      year : 2012,
+      images : [
+        "http://ia.media-imdb.com/images/M/MV5BMjIyNTQ5NjQ1OV5BMl5BanBnXkFtZTcwODg1MDU4OA@@._V1_SX640_SY720_.jpg"
+      ]
+    }, {
+      title : 'There Will Be Blood',
+      year : 2001,
+      images : [
+        "http://ia.media-imdb.com/images/M/MV5BMjA0NjE1ODEyNV5BMl5BanBnXkFtZTcwNDIzMzE5NQ@@._V1_SX640_SY720_.jpg"
+      ]
+  }];
 
 })();
